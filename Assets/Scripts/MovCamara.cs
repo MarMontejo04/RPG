0c2342c322748baea9ce549a25dd70b56b370f4c
Mar <mariana.montejo04@gmail.com>
2025-02-09 16:07:23 -0600

    Portales 1, 2, 5 y 6 añadidos, junto con script que permite el movimiento de camara

---
+++ Assets/Scripts/MovCamara.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovCamara : MonoBehaviour
+{
+    public Camera camara;
+
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        if(obj.gameObject.tag == "portal1") //Entrada a la aldea
+        {
+            Vector3 posicionCamara = new Vector3(0, 11.61f, -10);
+            camara.transform.position = posicionCamara;
+            Vector3 posicionPlayer = new Vector3(0, 8.21f, 0);
+            this.transform.position = posicionPlayer;
+
+        }
+
+        if (obj.gameObject.tag == "portal2") //Regreso a escenario inicial
+        {
+            Vector3 posicionCamara = new Vector3(0, -0.16f, -10);
+            camara.transform.position = posicionCamara;
+            Vector3 posicionPlayer = new Vector3(0, 2.97f, 0);
+            this.transform.position = posicionPlayer;
+
+        }
+
+        if (obj.gameObject.tag == "portal3")
+        {
+            Vector3 posicionCamara = new Vector3(1, 1, -10);
+            camara.transform.position = posicionCamara;
+            Vector3 posicionPlayer = new Vector3(1, 1, 1);
+            this.transform.position = posicionPlayer;
+
+        }
+
+        if (obj.gameObject.tag == "portal4")
+        {
+            Vector3 posicionCamara = new Vector3(1, 1, -10);
+            camara.transform.position = posicionCamara;
+            Vector3 posicionPlayer = new Vector3(1, 1, 1);
+            this.transform.position = posicionPlayer;
+
+        }
+
+        if (obj.gameObject.tag == "portal5") //Entrada a fabrica
+        {
+            Vector3 posicionCamara = new Vector3(-19.86f, 11.61f, -10);
+            camara.transform.position = posicionCamara;
+            Vector3 posicionPlayer = new Vector3(-12.84f, 10.11f, 0);
+            this.transform.position = posicionPlayer;
+
+        }
+
+        if (obj.gameObject.tag == "portal6") // Regreso a la aldea
+        {
+            Vector3 posicionCamara = new Vector3(0, 11.61f, -10);
+            camara.transform.position = posicionCamara;
+            Vector3 posicionPlayer = new Vector3(-7.24f, 11.94f, 0);
+            this.transform.position = posicionPlayer;
+
+        }
+
+        if (obj.gameObject.tag == "portal7")
+        {
+            Vector3 posicionCamara = new Vector3(1, 1, -10);
+            camara.transform.position = posicionCamara;
+            Vector3 posicionPlayer = new Vector3(1, 1, 1);
+            this.transform.position = posicionPlayer;
+
+        }
+
+        if (obj.gameObject.tag == "portal8")
+        {
+            Vector3 posicionCamara = new Vector3(1, 1, -10);
+            camara.transform.position = posicionCamara;
+            Vector3 posicionPlayer = new Vector3(1, 1, 1);
+            this.transform.position = posicionPlayer;
+
+        }
+
+    }
+}
