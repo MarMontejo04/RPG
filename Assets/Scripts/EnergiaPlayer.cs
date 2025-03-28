ded190cf400fd40caa76cf309f5a7c9a0dc4bec6
Mar <mariana.montejo04@gmail.com>
2025-03-28 09:55:39 -0600

    Script para la energia

---
+++ Assets/Scripts/EnergiaPlayer.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergiaPlayer : MonoBehaviour
+{
+    public Image energiaPlayer;
+    private float anchoEnergiaPlayer;
+    public static int energia; private bool sinEnergia;
+    private const int energiaINI = 10;
+    public static int puedePerderEnergia = 1;
+
+
+
+}
+
--- Assets/Scripts/VidasPlayer.cs
+++ Assets/Scripts/VidasPlayer.cs
@@ -15,36 +15,36 @@ public static int puedePerderVida = 1;
 
 void Start()
 {
-anchoVidasPlayer = vidaPlayer.GetComponent<RectTransform>().sizeDelta.x;
-haMuerto = false;
-vida = vidasINI;
-gameOver .SetActive(false);
+    anchoVidasPlayer = vidaPlayer.GetComponent<RectTransform>().sizeDelta.x;
+    haMuerto = false;
+    vida = vidasINI;
+    gameOver .SetActive(false);
 }
 
 public void TomarDano(int dano)
 {
-if (vida > 0 && puedePerderVida == 1) {
-puedePerderVida = 0;
-vida -= dano;
-DibujaVida(vida);
-}
-if
-(vida <= 0 && !haMuerto) 
-{
-haMuerto = true;
-StartCoroutine(EjecutaMuerte());
-}
+    if (vida > 0 && puedePerderVida == 1) {
+        puedePerderVida = 0;
+        vida -= dano;
+        DibujaVida(vida);
+    }
+
+    if(vida <= 0 && !haMuerto) 
+    {
+        haMuerto = true;
+        StartCoroutine(EjecutaMuerte());
+    }
 }
 
 
 public void DibujaVida (int vida) {
-RectTransform transformaImagen = vidaPlayer.GetComponent<RectTransform>();
-transformaImagen.sizeDelta = new Vector2(anchoVidasPlayer * (float)vida / (float)vidasINI, transformaImagen.sizeDelta.y);
+    RectTransform transformaImagen = vidaPlayer.GetComponent<RectTransform>();
+    transformaImagen.sizeDelta = new Vector2(anchoVidasPlayer * (float)vida / (float)vidasINI, transformaImagen.sizeDelta.y);
 }
 
 IEnumerator EjecutaMuerte()
 {
-yield return new WaitForSeconds (2.1f); 
-gameOver .SetActive(true);
+    yield return new WaitForSeconds (2.1f); 
+    gameOver .SetActive(true);
 }
 }
