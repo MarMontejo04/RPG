bdb65ff1755ffb98b4f693e82d4f3c74152bc490
Mar <mariana.montejo04@gmail.com>
2025-04-08 19:46:13 -0600

    Menú pausa completo

--- Assets/Scripts/MenuInicial.cs
+++ Assets/Scripts/MenuInicial.cs
@@ -6,13 +6,13 @@ using UnityEngine.SceneManagement;
 public class MenuInicial : MonoBehaviour
 {
 
-public void Jugar()
-{
-SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-}
+    public void Jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
-public void Salir(){
-Debug.Log("Salir...");
-Application.Quit();
-}
+    public void Salir(){
+        Debug.Log("Salir...");
+        Application.Quit();
+    }
 }
--- Assets/Scripts/MenuPausa.cs
+++ Assets/Scripts/MenuPausa.cs
@@ -1,6 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class MenuPausa : MonoBehaviour
 {
+    public GameObject menuPausa;
+    public bool juegoPausado = false;
+    [SerializeField] private AudioMixer audioMixer;
     
+    public void Reanudar(){
+        menuPausa.SetActive(false);
+        Time.timeScale = 1;
+        juegoPausado = false;
+    }
+    public void Pausar(){
+        menuPausa.SetActive(true);
+        Time.timeScale = 0;
+        juegoPausado = true;
+    }
+    public void Salir(){
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+    }
+     public void CambiarVolumen(float volumen)
+    {
+        audioMixer.SetFloat("Volumen", volumen);
+    }
+
 }
