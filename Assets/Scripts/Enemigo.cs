9078cc42693c748da4d4e5a7929afdce95f00a2c
ojoninja03 <Unacuentam126@gmail.com>
2025-04-24 21:26:32 -0600

    aaaaaa
    aaaaaaaa

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -1,6 +1,6 @@
 using System;
-using System. Collections;
-using System. Collections. Generic;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,75 +9,65 @@ public class Enemigo : MonoBehaviour
     public static int vidaEnemigo = 1;
     private float frecAtaque = 2.5f, tiempoSigAtaque = 0, iniciaConteo;
     private Animator animator;
-    private Vector2 dirMov;
     public float velMov;
     public Rigidbody2D rb;
     public Animator anim;
     public Transform personaje;
     private NavMeshAgent agente;
-    public Transform [] puntosRuta;
+    public Transform[] puntosRuta;
     private int indiceRuta;
     private bool playerEnRango = false;
     [SerializeField] private float distanciaDeteccionPlayer;
-    private SpriteRenderer spriteEnemigo;
-    private Transform mirarHacia;
     [SerializeField] private GameObject dropeable;
 
     private void Awake()
     {
         agente = GetComponent<NavMeshAgent>();
-        spriteEnemigo = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
-    }
+   }
 
-    void Start() {
+    void Start()
+    {
         vidaEnemigo = 1;
         agente.updateRotation = false;
         agente.updateUpAxis = false;
     }
 
-    void FixedUpdate() {
-        Movimiento();
+    void FixedUpdate()
+    {
         ActualizarAnimaciones();
     }
 
-    private void ActualizarAnimaciones() {
+    private void ActualizarAnimaciones()
+    {
         float velocidad = agente.velocity.magnitude;
-        animator.SetBool("IsMover", velocidad > 0.1f);
-        if(velocidad >= 0.1)
+        animator.SetBool("isMover", velocidad > 0.1f);
+        if (velocidad >= 0.1f)
         {
             Vector3 direccionMovimiento = agente.velocity.normalized;
             animator.SetFloat("movX", direccionMovimiento.x);
             animator.SetFloat("movY", direccionMovimiento.y);
         }
-
     }
 
-    private void Movimiento() {
-        float movX = Input.GetAxisRaw("Horizontal");
-        float movY = Input.GetAxisRaw("Vertical");
-        dirMov = new Vector2(movX, movY).normalized;
-        rb.linearVelocity = new Vector2(dirMov.x * velMov, dirMov.y * velMov);
-
-    }
-
-    void Update() {
+    void Update()
+  {
         this.transform.position = new Vector3(transform.position.x, transform.position.y, 0);
         float distancia = Vector3.Distance(personaje.position, this.transform.position);
 
-        if(Vector3.Distance(this.transform.position, puntosRuta[indiceRuta].position) < 0.1f)
+        if (Vector3.Distance(this.transform.position, puntosRuta[indiceRuta].position) < 0.1f)
         {
-            if(indiceRuta < puntosRuta.Length - 1)
+            if (indiceRuta < puntosRuta.Length - 1)
             {
                 indiceRuta++;
             }
-            else if(indiceRuta == puntosRuta.Length - 1)
+            else if (indiceRuta == puntosRuta.Length - 1)
             {
                 indiceRuta = 0;
             }
         }
 
-        if(distancia < distanciaDeteccionPlayer)
+        if (distancia < distanciaDeteccionPlayer)
         {
             playerEnRango = true;
         }
@@ -86,16 +76,15 @@ public class Enemigo : MonoBehaviour
             playerEnRango = false;
         }
 
-        if(tiempoSigAtaque > 0)
+        if (tiempoSigAtaque > 0)
         {
-            tiempoSigAtaque = frecAtaque + iniciaConteo - Time.time;
+          tiempoSigAtaque = frecAtaque + iniciaConteo - Time.time;
         }
         else
         {
             tiempoSigAtaque = 0;
             VidasPlayer.puedePerderVida = 1;
             SigueAlPlayer(playerEnRango);
-            RotaEnemigo();
         }
     }
 
@@ -104,45 +93,31 @@ public class Enemigo : MonoBehaviour
         if (playerEnRango)
         {
             agente.SetDestination(personaje.position);
-            mirarHacia = personaje;
         }
         else
         {
             agente.SetDestination(puntosRuta[indiceRuta].position);
-            mirarHacia = puntosRuta[indiceRuta];
         }
     }
 
-    private void RotaEnemigo()
+    private void OnTriggerEnter2D(Collider2D obj)
     {
-        if(this.transform.position.x > mirarHacia.position.x)
+        if (obj.tag == "Player")
         {
-            spriteEnemigo.flipX = true;
-            Debug.Log("FlipX");
-        }
-        else
-        {
-            spriteEnemigo.flipX = false;
-            Debug.Log("Sin FlipX");
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D obj) {
-     if (obj.tag == "Player") {
             tiempoSigAtaque = frecAtaque;
             iniciaConteo = Time.time;
             obj.transform.GetComponentInChildren<VidasPlayer>().TomarDano(1);
-      }
-    }   
-
-    public void TomarDano(int dano) {
-        vidaEnemigo -= dano;
-        if (vidaEnemigo <= 0) {
-             Vector2 v2Prefab = this.transform.position; 
-             Instantiate(dropeable, v2Prefab, transform.rotation);
-            Destroy(gameObject);
         }
-    }
-    
+   }
 
+    public void TomarDano(int dano)
+    {
+        vidaEnemigo -= dano;
+        if (vidaEnemigo <= 0)
+        {
+            Vector2 v2Prefab = this.transform.position;
+            Instantiate(dropeable, v2Prefab, transform.rotation);
+            Destroy(gameObject);
+            }
+        }
 }
